48b35e4c99bd504dd0e175d395e6aca2c09ba25e
why <2586244885@qq.com>
2023-08-08 17:52:18 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
+<script lang="ts" setup>
+import { testApi } from './api' // note this in volar 1.8.8 or 1.2.0
+
+const name = ref('volar')
+
+testApi()
+</script>
+
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <RouterView />
-    <TheFooter />
-  </main>
+  <div class="h-screen w-screen flex items-center justify-center text-6xl">
+    test {{ name }} fast fix
+  </div>
 </template>
---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import type { TestType } from '~/type' // note this in volar 1.8.8 or 1.2.0
+
+interface Test2Type extends TestType {
+  //
+}
+
+export function testApi() {
+  // console.log('test api')
+}
