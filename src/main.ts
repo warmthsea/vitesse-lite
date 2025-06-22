8f84302cac97322971ef46a0c0c20a8abbc4c0bc
warmthsea <2586244885@qq.com>
2025-06-22 22:26:15 +0800

    chore: use tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
 
+import './styles/index.css'
 import './styles/main.css'
-import 'uno.css'
 
 const app = createApp(App)
 const router = createRouter({
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 
 import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import Vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
@@ -56,9 +56,7 @@ export default defineConfig({
       dts: true,
     }),
 
-    // https://github.com/antfu/unocss
-    // see uno.config.ts for config
-    UnoCSS(),
+    tailwindcss(),
   ],
 
   // https://github.com/vitest-dev/vitest
